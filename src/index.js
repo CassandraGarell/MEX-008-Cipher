bd1077704ddf38c00a1896c73247370c6a8c2961
CassandraGarell <cassandragarell@gmail.com>
2019-06-19 16:36:56 -0500

    Subiendo actualizacion del html para Cipher

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const nombreColonia = document.getElementById('nombreColonia');
-
-const getNeigtborhood = () => {
-   primerSeccion.classList.add("desaparecer");
-   content.classList.remove("desaparecer")
-   content.innerHTML = "Hola vecino de"+ " " + nombreColonia.value + "recibirás a continuación instrucciones para generar tu código de denuncia anónima"
-}
+// const nombreColonia = document.getElementById('nombreColonia');
+//
+// const getNeigtborhood = () => {
+//    primerSeccion.classList.add("desaparecer");
+//    content.classList.remove("desaparecer")
+//    content.innerHTML = "Hola vecino de"+ " " + nombreColonia.value + "recibirás a continuación instrucciones para generar tu código de denuncia anónima"
+// }
